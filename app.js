0c5577b3ce1127e530eb2ff325761867a73a93d9
georgebzhang <georgebzhang5@gmail.com>
2019-05-27 13:40:48 -0700

    2-13

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const Logger = require('./logger');
-const logger = new Logger();
+const http = require('http');
 
-// Register a listener
-logger.on('messageLogged', arg => {
-	console.log('Listener called', arg);
+const server = http.createServer((req, res) => {
+	if (req.url === '/') {
+		res.write('Hello world');
+		res.end();
+	}
+
+	if (req.url === '/api/courses') {
+		res.write(JSON.stringify([1, 2, 3]));
+		res.end();
+	}
 });
 
-logger.log('message');
+// server.on('connection', socket => {
+// 	console.log('New connection...');
+// });
+
+server.listen(3000);
+console.log('Listening on port 3000...');
