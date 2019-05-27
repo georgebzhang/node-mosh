dc4c1840125d99740dc65f5c1a95af21bda59473
georgebzhang <georgebzhang5@gmail.com>
2019-05-27 16:54:40 -0700

    4-9

--- app.js
+++ app.js
@@ -12,9 +12,5 @@ const server = http.createServer((req, res) => {
 	}
 });
 
-// server.on('connection', socket => {
-// 	console.log('New connection...');
-// });
-
 server.listen(3000);
 console.log('Listening on port 3000...');
