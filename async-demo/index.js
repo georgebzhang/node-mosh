c287bad89acb5ee6a9b26289ce0e405e1287c2ca
georgebzhang <georgebzhang5@gmail.com>
2019-05-28 10:55:55 -0700

    6-3

---
+++ async-demo/index.js
@@ -0,0 +1,24 @@
+console.log('Before');
+
+getUser(1, user => {
+	console.log('User', user);
+	getRepositories(user.githubUsername, repos => {
+		console.log(repos);
+	});
+});
+
+console.log('After');
+
+function getUser(id, callback) {
+	setTimeout(() => {
+		console.log('Reading a user from a database...');
+		callback({id: id, githubUsername: 'george'});
+	}, 2000);
+}
+
+function getRepositories(username, callback) {
+	setTimeout(() => {
+		console.log('Calling GitHub API...');
+		callback(['repo1', 'repo2', 'repo3']);
+	}, 2000);
+}
