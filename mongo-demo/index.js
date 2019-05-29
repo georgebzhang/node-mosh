ef92ebb07d54d9118bc5f057439a894a1ff876cb
georgebzhang <georgebzhang5@gmail.com>
2019-05-29 09:24:06 -0700

    8-4

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -21,8 +21,13 @@ const courseSchema = new mongoose.Schema({
 	tags: {
 		type: Array,
 		validate: { // custom validator
-			validator: function(v) {
-				return v && v.length > 0;
+			isAsync: true,
+			validator: function(v, callback) {
+				setTimeout(() => {
+					// Do async work
+					const result = v && v.length > 0;
+					callback(result);
+				}, 2000);
 			},
 			message: 'A course should have at least one tag.'
 		}
