f23c435bb1287c7dd26005952042732b19ec2ea7
georgebzhang <georgebzhang5@gmail.com>
2019-05-28 19:01:53 -0700

    6-11

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,12 +1,26 @@
 console.log('Before');
 
+// Promise approach
 getUser(1)
 	.then(user => getRepositories(user.githubUsername))
 	.then(repos => console.log('Repositories: ', repos))
-	.catch(err => console.log('Error', err));
+	.catch(err => console.log('Error', err.message));
+
+// async/await approach
+async function displayRepositories() {
+	try {
+		const user = await getUser(1);
+		const repos = await getRepositories(user.githubUsername);
+		console.log('Repositories: ', repos);
+	} catch (err) {
+		console.log('Error', err.message);
+	}
+}
+displayRepositories();
 
 console.log('After');
 
+//functions
 function getUser(id) {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
@@ -20,7 +34,8 @@ function getRepositories(username) {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
 			console.log('Calling GitHub API...');
-			resolve(['repo1', 'repo2', 'repo3']);
+			// resolve(['repo1', 'repo2', 'repo3']);
+			reject(new Error('Could not get repos'));
 		}, 2000);
 	});
 }
