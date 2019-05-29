b00a4b99d1ace5bb7ef0485aeb2bee4a92e2911a
georgebzhang <georgebzhang5@gmail.com>
2019-05-28 18:33:51 -0700

    6-10

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,5 +1,19 @@
-// const p = Promise.resolve({id: 1})
-const p = Promise.reject(new Error('Error message'));
+const p1 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		console.log('Async operation 1...');
+		resolve(1);
+		// reject(new Error('Something failed'));
+	}, 2000);
+});
 
-p	.then(result => console.log(result))
-	.catch(error => console.log(error));
+const p2 = new Promise(resolve => {
+	setTimeout(() => {
+		console.log('Async operation 2...');
+		resolve(2);
+	}, 2000);
+});
+
+// Promise.all([p1, p2])
+Promise.race([p1, p2])
+	.then(result => console.log(result))
+	.catch(err => console.log('Error: ', err.message));
